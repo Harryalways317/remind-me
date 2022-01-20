3d18dc419371ca02e95a673c2807095d3b96a975
Harish Vadaparty <harishvadaparty@gmail.com>
2022-01-20 21:45:21 +0530

    implemented hive local DB

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,27 +1,49 @@
 import 'package:flutter/foundation.dart';
+import 'package:hive/hive.dart';
 import 'package:remindme/models/task.dart';
 
 class TasksData extends ChangeNotifier{
-  List<Task> tasksList = [
-    Task(isChecked: false,name: "Task1",creationDate: DateTime.now()),
+  // List<Task> tasksList = [
+  //   Task(isChecked: false,name: "Task1",creationDate: DateTime.now()),
+  //
+  // ];
 
-  ];
+
+
+  Box<Task> tasksBox  = Hive.box<Task>('TODOs');
+
+
+
+
 
 
 
   addTasks({required String name, required bool isChecked,required DateTime creationDate}){
-    tasksList.add(Task(name: name, isChecked: isChecked, creationDate: creationDate));
+    //tasksList.add(Task(name: name, isChecked: isChecked, creationDate: creationDate));
+    tasksBox.add(Task(name: name, isChecked: isChecked, creationDate: creationDate));
 
     notifyListeners();
   }
 
   deleteTasks(Task task){
-    tasksList.remove(task);
+    //tasksList.remove(task);
+    tasksBox.delete(task);
     notifyListeners();
   }
   
-  toggleTask(Task task){
+  toggleTask(Task task,int index)async{
+
+
+
+
+    tasksBox.deleteAt(index);
+
     task.toggleTaskCheck();
+
+   tasksBox.add(task);
+    notifyListeners();
+
+
 
     notifyListeners();
   }
